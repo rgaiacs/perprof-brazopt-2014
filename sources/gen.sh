ec62bf4a152584511b66b52ea52b074a63bbf733
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-03-10 21:12:20 -0300

    Update images to standalone and add sources to generate them

--- sources/gen.sh
+++ sources/gen.sh
@@ -6,6 +6,7 @@ $cmd -o abc
 $cmd --semilog -o abc-semilog
 $cmd --black-and-white --semilog -o abc-bw-semilog
 $cmd --semilog -o abc-100 --tau 100
+$cmd --semilog -o abc-hs --subset hs.subset
 
 latexmk -c
 mv *.pdf ../
